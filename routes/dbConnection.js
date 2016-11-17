eaa1d058023ec32a354506ebcbcbfa89ee9e5b93
Developer I <developeri@iM-L13-693.local>
2016-11-17 10:15:51 -0600

    Set format code

--- routes/dbConnection.js
+++ routes/dbConnection.js
@@ -1,5 +1,5 @@
-var MongoClient = require('mongodb').MongoClient;
-var express = require('express');
+var MongoClient = require("mongodb").MongoClient;
+var express = require("express");
 var router = express.Router();
 var Q = require("q");
 
@@ -7,46 +7,46 @@ var database = this;
 
 // Create a user
 function createUser(req,res,next){
-	console.log(req.body);
-	var user = req.user;
-	if(user.firstName && user.lastName && user.userName && user.password){
-		database.db.User.insert(user);
-		res.send({
-			user : user,
-			message : 'User ' + user.userName + ' has been created successfully.'
-		});
-	}else{
-		res.send({message:'You most include first name, last name, username and password at less'});
-	}
+    console.log(req.body);
+    var user = req.user;
+    if(user.firstName && user.lastName && user.userName && user.password){
+        database.db.User.insert(user);
+        res.send({
+            user : user,
+            message : "User " + user.userName + " has been created successfully."
+        });
+    }else{
+        res.send({message:"You most include first name, last name, username and password at less"});
+    }
 };
 
 function showUsers(req,res,next){
-	res.send("It is the users list");
+    res.send("It is the users list");
 }
 
 
-router.get('/',showUsers);
+router.get("/",showUsers);
 
-router.post('/createUser', createUser);
+router.post("/createUser", createUser);
 
 // Method to get connection with mongo db
-database.connection = function(url='mongodb://admin:4dm1n@localhost:27017/payment'){
-	var deferred = Q.defer();
-	MongoClient.connect(url,function(err,db){
-		if(err){
-			console.log("Error has been occurred: "+err);
-			deferred.reject(false);
-		}else{
-			deferred.resolve(true);
-			console.log("The connection with database has been success.");
-			database.db = db;
-		}
-	});
+database.connection = function(url="mongodb://admin:4dm1n@localhost:27017/payment"){
+    var deferred = Q.defer();
+    MongoClient.connect(url,function(err,db){
+        if(err){
+            console.log("Error has been occurred: "+err);
+            deferred.reject(false);
+        }else{
+            deferred.resolve(true);
+            console.log("The connection with database has been success.");
+            database.db = db;
+        }
+    });
 
-	return deferred.promise;
+    return deferred.promise;
 };
 
 module.exports = {
-	database : database,
-	router : router
+    database : database,
+    router : router
 };
